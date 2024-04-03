96b6dddd9b6a439231dbd2aa1cff9e588d8a2d1f
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-04-03 19:52:32 +0200

    add all the platforms

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+mkdir build
+cd build
+
+cmake %CMAKE_ARGS% ^
+      -GNinja ^
+      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -DCMAKE_PREFIX_PATH=%PREFIX% ^
+      -DLWS_UNIX_SOCK=ON ^
+      -DLWS_WITH_STATIC=OFF ^
+      -DLWS_WITHOUT_TESTAPPS=ON ^
+      -DLWS_WITH_HTTP_PROXY=ON ^
+      -DLWS_WITH_ACCESS_LOG=ON ^
+      -DLWS_WITH_LIBUV=ON ^
+      -DLWS_WITH_SERVER_STATUS=ON ^
+      ..
+
+ninja install
